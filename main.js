725ef855da8c1118dbf37086bbc814e53cc82994
LucaMarchesani <lucamarchesani.lavoro@gmail.com>
2022-12-12 17:49:44 +0100

    small aesthetic changes

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
-const {createApp} = Vue;
+const { createApp } = Vue;
 
 createApp({
     data() {
         return {
-            message: 'Vue is here!'
+            title: 'ue is here!',
+            text: 'Vue.js (commonly referred to as Vue; pronounced "view") is an open-source model-view-viewmodel front end JavaScript framework for building user interfaces and single-page applications. It was created by Evan You, and is maintained by him and the rest of the active core team members.',
+            image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.giuseppemaccario.com%2Fwp-content%2Fuploads%2F2019%2F04%2F1200px-Vue.js_Logo_2.svg_-1024x887.png&f=1&nofb=1&ipt=ec44d02f1ff33854dfb186b4a1ebc249ca756fcb857b6d3bc1a3ddf2bc5cfde7&ipo=images'
         }
     }
-}) .mount('#app');
+}).mount('#app');
+
+
