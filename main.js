1bbdd5c08e45eb7a7099ffd6b3cb23bb96f7bb26
LucaMarchesani <lucamarchesani.lavoro@gmail.com>
2022-12-12 19:32:38 +0100

    add counter

--- main.js
+++ main.js
@@ -5,9 +5,15 @@ createApp({
         return {
             title: 'ue is here!',
             text: 'Vue.js (commonly referred to as Vue; pronounced "view") is an open-source model-view-viewmodel front end JavaScript framework for building user interfaces and single-page applications. It was created by Evan You, and is maintained by him and the rest of the active core team members.',
-            image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.giuseppemaccario.com%2Fwp-content%2Fuploads%2F2019%2F04%2F1200px-Vue.js_Logo_2.svg_-1024x887.png&f=1&nofb=1&ipt=ec44d02f1ff33854dfb186b4a1ebc249ca756fcb857b6d3bc1a3ddf2bc5cfde7&ipo=images'
+            image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.giuseppemaccario.com%2Fwp-content%2Fuploads%2F2019%2F04%2F1200px-Vue.js_Logo_2.svg_-1024x887.png&f=1&nofb=1&ipt=ec44d02f1ff33854dfb186b4a1ebc249ca756fcb857b6d3bc1a3ddf2bc5cfde7&ipo=images',
+            counterIncr: 'Clicca qui per incrementare il counter',
+            counterDecr: 'Clicca qui per decrementare il counter',
+            counter: 0
         }
     }
 }).mount('#app');
+
+
+
 
 
